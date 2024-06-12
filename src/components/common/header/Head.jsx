cc9c86fe6ad4ed601a6798e56f649b3b78d45240
Durgaprasad Dalai <durgaprasaddalai10@gmail.com>
2024-06-12 18:58:26 +0530

    head change

--- src/components/common/header/Head.jsx
+++ src/components/common/header/Head.jsx
@@ -1,25 +1,27 @@
-import React from "react"
-
+import React from "react";
+import { Link } from "react-router-dom";
 const Head = () => {
   return (
     <>
-      <section className='head'>
-        <div className='container flexSB'>
-          <div className='logo'>
-            <h1>  GURUKRUPA</h1>
-            <span>RESEARCH AND TRAINING CENTRE</span>
-          </div>
+      <section className="head">
+        <div className="container flexSB">
+          <Link to="/">
+            <div className="logo" style={{color:'#ffff'}}>
+              <h1> GURUKRUPA</h1>
+              <span>RESEARCH AND TRAINING CENTRE</span>
+            </div>
+          </Link>
 
-          <div className='social'>
-            <i className='fab fa-facebook-f icon'></i>
-            <i className='fab fa-instagram icon'></i>
-            <i className='fab fa-twitter icon'></i>
-            <i className='fab fa-youtube icon'></i>
+          <div className="social">
+            <i className="fab fa-facebook-f icon"></i>
+            <i className="fab fa-instagram icon"></i>
+            <i className="fab fa-twitter icon"></i>
+            <i className="fab fa-youtube icon"></i>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Head
+export default Head;
